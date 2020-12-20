ad75067bf328fcb352c11692c9cf4dd3b24aa1b6
pikokr <parangee9706@gmail.com>
2020-12-20 22:14:24 +0900

    feat: change font size of name, add brief

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -5,7 +5,8 @@ class HomePage extends Component {
     render() {
         return (
             <div className={classes.root}>
-                <h1>파랑이</h1>
+                <h1 className={classes.name}>파랑이</h1>
+                <h2>UniqueCode 소속 개발자</h2>
             </div>
         );
     }
