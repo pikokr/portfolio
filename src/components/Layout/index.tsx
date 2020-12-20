dbc659608c4b6a4406168bc68e11898e9cbdd767
pikokr <parangee9706@gmail.com>
2020-12-20 21:56:04 +0900

    fix: content not showing because of header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import classes from './Layout.module.scss'
 
 class Layout extends Component {
     render() {
         return (
             <>
                 <Header/>
-                {this.props.children}
+                <div style={{height: 80}}/>
+                <div className={classes.container}>
+                    {this.props.children}
+                </div>
             </>
         );
     }
