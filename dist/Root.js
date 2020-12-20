dd226f5575743fc0f3a3d5c95ea352a88a34b029
pikokr <parangee9706@gmail.com>
2020-12-20 11:43:36 +0000

    commit 071363cfb9dab0d839bfe9cf0b9ea82d261230d0 Author: pikokr <parangee9706@gmail.com> Date:   Sun Dec 20 20:42:46 2020 +0900
        feat: add .idea to ignore

---
+++ dist/Root.js
@@ -0,0 +1,6 @@
+import React from "../web_modules/react.js";
+import {BrowserRouter} from "../web_modules/react-router-dom.js";
+const Root = () => {
+  return /* @__PURE__ */ React.createElement(BrowserRouter, null, "ROOT");
+};
+export default Root;
--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,9 @@ import.meta.env = __SNOWPACK_ENV__;
 
 import React from "../web_modules/react.js";
 import ReactDOM from "../web_modules/react-dom.js";
-import App2 from "./App.js";
+import Root2 from "./Root.js";
 import "./index.css.proxy.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App2, null)), document.getElementById("root"));
+ReactDOM.render(/* @__PURE__ */ React.createElement(Root2, null), document.getElementById("root"));
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
