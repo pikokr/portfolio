bab1b5dcf3582a7c610495a1048875db6826adb6
pikokr <parangee9706@gmail.com>
2020-12-21 08:52:14 +0900

    feat: button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import styled from 'styled-components'
 
-const Button = ((props) => {
-  return (
-    <button {...props}>{props.children}</button>
-  );
-}) as React.FC<HTMLButtonElement & any>;
+const Button = styled.a`
+border: 1px solid #000;
+padding: 10px;
+user-select: none;
+cursor: pointer;
+color: #000;
+&:hover {
+    background: #000;
+    color: #fff;
+}
+`
 
 export default Button;
