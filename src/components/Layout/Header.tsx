fce9f73a516cd6fa4168b4566554097120560412
pikokr <parangee9706@gmail.com>
2020-12-20 21:25:32 +0900

    feat: header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './Header.module.scss'
 
 const Header = () => {
     return (
         <div className={classes.container}>
-            헤더
+            <h2>헤더</h2>
+            <div style={{flexGrow: 1}}/>
+            <ul className={classes.menuContainer}>
+                <li>
+                    <Link to="/">About</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+            </ul>
         </div>
     );
 };
