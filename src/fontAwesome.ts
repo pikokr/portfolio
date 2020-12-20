9aa9a328a0e147f0e6989e24c2a828190b127782
pikokr <parangee9706@gmail.com>
2020-12-20 22:31:26 +0900

    feat: activate fontawesome

---
+++ src/fontAwesome.ts
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faGithub)
+library.add(faDiscord)
+library.add(faEnvelope)
