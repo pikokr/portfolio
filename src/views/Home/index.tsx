35c11c1b68bfe51287ca556285c74a558d3612a1
pikokr <parangee9706@gmail.com>
2020-12-21 10:57:39 +0900

    fix: button style with href

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../../components/Button';
+import * as Button from '../../components/Button';
 import classes from './home.module.scss';
 
 class HomePage extends Component {
@@ -9,9 +8,7 @@ class HomePage extends Component {
       <div className={classes.root}>
         <h1 className={classes.name}>파랑이</h1>
         <h2>UniqueCode 소속 개발자</h2>
-        <Link to="/about">
-          <Button>더 알아보기</Button>
-        </Link>
+        <Button.Link to="/about">더 알아보기</Button.Link>
       </div>
     );
   }
