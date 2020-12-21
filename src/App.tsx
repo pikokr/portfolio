143939f5a853f5fba4fc3afbdf1adb2fe16538c2
pikokr <parangee9706@gmail.com>
2020-12-21 11:07:39 +0900

    feat: about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
+import AboutPage from './views/About';
 import HomePage from './views/Home';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Layout>
           <Switch location={location}>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
           </Switch>
         </Layout>
       )}
