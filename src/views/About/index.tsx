2c590b44e8ed85f0e05c6a2eee9069d5b5f187b2
pikokr <parangee9706@gmail.com>
2020-12-26 13:17:15 +0900

    feat: programming languages list

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -8,7 +8,18 @@ class AboutPage extends Component {
             <div>
                 <h1>파랑이에 대해 더 알아볼까요?</h1>
                 <div className={styles.aboutContainer}>
-                    <AboutCard title="프로그래밍" icon={['fas', 'code']} desc="테스트"/>
+                    <AboutCard title="프로그래밍" icon={['fas', 'code']} desc={
+                      <div>
+                        파랑이가 할수 있는 프로그래밍 언어들
+                        <p>- Javascript</p>
+                        <p>- Typescript</p>
+                        <p>- Kotlin</p>
+                        <p>- PHP(안씀)</p>
+                        <p>- C,C#,C++(안씀)</p>
+                        <p>- Java(안씀)</p>
+                        <p>- Python(안씀)</p>
+                      </div>
+                    }/>
                 </div>
             </div>
         );
