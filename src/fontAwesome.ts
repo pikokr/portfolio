c56600b2b3dcd7970b960f9980dac6a98e9aaad2
pikokr <parangee9706@gmail.com>
2020-12-26 12:45:59 +0900

    feat: backup

--- src/fontAwesome.ts
+++ src/fontAwesome.ts
@@ -1,8 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faBars, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCode, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGithub);
-library.add(faDiscord);
-library.add(faEnvelope);
-library.add(faBars);
+library.add(faGithub, faDiscord, faBars, faEnvelope, faCode);
--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import AboutCard from '../../components/AboutCard';
+import styles from './about.module.sass'
 
 class AboutPage extends Component {
     render() {
         return (
             <div>
                 <h1>파랑이에 대해 더 알아볼까요?</h1>
+                <div className={styles.aboutContainer}>
+                    <AboutCard title="프로그래밍" icon={['fas', 'code']} desc="테스트"/>
+                </div>
             </div>
         );
     }
