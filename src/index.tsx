296e3749b2af3fc1b7f5a787fa06984f5b284094
pikokr <parangee9706@gmail.com>
2021-01-18 10:18:38 +0900

    :sparkles: init tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import './index.css'
+import Root from './Root'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root'),
 )
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept()
 }
