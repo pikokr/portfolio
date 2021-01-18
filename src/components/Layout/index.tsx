7833430a0b8d4104a9e09edcc0953fa9cbf9fda0
pikokr <parangee9706@gmail.com>
2021-01-18 11:47:21 +0900

    :sparkles: add some links to footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Header from './Header'
 
 const Layout: React.FC = ({ children }) => {
@@ -16,8 +17,16 @@ const Layout: React.FC = ({ children }) => {
           {children}
         </div>
         <div className="flex-grow" />
-        <div className="p-4 w-full border-t text-right">
-          2020 &copy; PIKOSTUDIO
+        <div className="p-4 w-full border-t grid md:grid-cols-3">
+          <div className="flex flex-col">
+            <Link to="/">홈</Link>
+            <Link to="/about">소개</Link>
+          </div>
+          <div className="flex flex-col">
+            <Link to="/">프로젝트</Link>
+            <Link to="/about">디스코드</Link>
+          </div>
+          <div className="md:text-right">2020 &copy; PIKOSTUDIO</div>
         </div>
       </div>
     </div>
