d60cacdd418371c91ee3e9493e278ebff0905e95
pikokr <parangee9706@gmail.com>
2021-01-18 11:35:43 +0900

    :sparkles: add link to home on sidebar

--- src/components/Layout/SideBar.tsx
+++ src/components/Layout/SideBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SideBar: React.FC<{
   onClose: () => void
@@ -17,12 +19,24 @@ const SideBar: React.FC<{
         className="bg-black opacity-50 fixed w-full h-full left-0 top-0"
       />
       <div
-        className={clsx('fixed h-full bg-white border-l transition-all', {
-          '-right-full': !open,
-          'right-0': open,
-        })}
+        className={clsx(
+          'fixed h-full bg-white border-l transition-all flex flex-col',
+          {
+            '-right-full': !open,
+            'right-0': open,
+          },
+        )}
+        style={{
+          minWidth: '100px',
+        }}
       >
-        사이드바
+        <Link
+          to="/"
+          className="p-2 hover:bg-gray-200 transition-colors w-full"
+          onClick={onClose}
+        >
+          <FontAwesomeIcon icon={['fas', 'home']} className="mr-2" />홈
+        </Link>
       </div>
     </div>
   )
